4fcc54209f4bce7aa4b16bd40f8f8624c6173357
Demidog03 <olzhasktl362@gmail.com>
2024-03-14 21:29:11 +0600

    удаление всего кроме dist

--- src/scripts/script.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-async function getData() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/photos')
-    console.log(response.data);
-}
-
-getData()
